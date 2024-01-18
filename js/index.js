8bb7cb3824bb03402dfa12ea51dc9744fade9602
edvard01 <frejaclueless@gmail.com>
2024-01-18 14:28:19 +0100

    All objects added

---
+++ js/index.js
@@ -0,0 +1,75 @@
+//School
+let lexicon = {
+  name: "lexicon",
+  address: "Göteborgsgatan 44",
+  zipcode: "432 12",
+  city: "göteborg",
+  students: [],
+  teachers: [],
+};
+
+//Subjects
+let matematik = {
+  name: "Matematik",
+  students: [],
+  teacher: {},
+};
+
+let idrott = {
+  name: "Idrott",
+  students: [],
+  teacher: {},
+};
+
+let svenska = {
+  name: "Svenska",
+  students: [],
+  teacher: {},
+};
+
+//Students
+let anna = {
+  name: "Anna",
+  age: 18,
+  gender: "tjej",
+  subjects: [],
+};
+
+let stellan = {
+  name: "Stellan",
+  age: 18,
+  gender: "kille",
+  subjects: [],
+};
+
+let stella = {
+  name: "Stella",
+  age: 19,
+  gender: "tjej",
+  subjects: [],
+};
+
+let mattias = {
+  name: "Mattias",
+  age: 18,
+  gender: "kille",
+  subjects: [],
+};
+
+let hugo = {
+  name: "Hugo",
+  age: 19,
+  gender: "kille",
+  subjects: [],
+};
+
+//Teachers
+let sara = {
+  name: "Sara",
+  subjects: [],
+};
+
+let bo = {
+  name: "Bo",
+  subjects: [],
+};
